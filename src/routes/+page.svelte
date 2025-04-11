5362a11c25bee447ea619b74b4a21b6cdf1a70fb
João Lucas Hoffman <contato.jlhoffman@gmail.com>
2025-04-11 01:04:54 -0300

    base css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,4 +12,43 @@
     font-family: "PA NicoMoji";
     src: url("/NicoMoji-Regular.ttf");
 }
+
+:global(:root) {
+    font-family: "PA Lexend";
+    font-weight: 200;
+}
+
+:global(*) {
+        margin: 0;
+        padding: 0;
+        border: none;
+        outline: none;
+    }
+
+:global(body) {
+    background: 
+    radial-gradient(circle at center, #454545aa 0%, #45454522 60%, transparent 100%),
+    linear-gradient(0deg, #1E1E1E 0%, #151515 50%, #101010 100%);
+    min-width: 100vw;
+    min-height: 100vh;
+    overflow: hidden;
+
+    &::after {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        backdrop-filter: blur(50px);
+    }
+}
+
+main {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
 </style>
